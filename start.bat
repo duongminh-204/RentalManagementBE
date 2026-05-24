aa6f0e7ed3047441917503167303878506bd0287
MINHDQHe187308 <minhduongqh0311@gmail.com>
2026-05-24 11:46:28 +0700

    config lại docker

--- start.bat
+++ start.bat
@@ -1,15 +1,23 @@
-﻿@echo off
+﻿﻿@echo off
+setlocal
+cd /d "%~dp0"
 
 echo =========================
-echo Starting Rental API...
+echo Pulling & Starting Rental API...
 echo =========================
 
+docker compose down
+docker compose pull
 docker compose up -d
 
 echo.
 echo =========================
+echo Waiting for API startup...
+timeout /t 10 >nul
+
+echo =========================
 echo API Running:
-echo http://localhost:8080/swagger
+echo http://localhost:8090/swagger
 echo =========================
 
 pause
